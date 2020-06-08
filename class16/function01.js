372c0d1c6412a447205dd5de583b6c5e93c7bb59
Wanderson Vicente <wanderson.vs@outlook.com>
2020-06-08 20:12:01 -0300

    Function on JavaScript

---
+++ class16/function01.js
@@ -0,0 +1,14 @@
+function parimpar(n) {
+    if (n%2==0){
+        return 'Par!'
+    }else{
+        return 'Ímpar!'
+    }
+
+}
+
+console.log(parimpar(557))
+/*
+let res = parimpar(4)
+console.log(res)
+*/
